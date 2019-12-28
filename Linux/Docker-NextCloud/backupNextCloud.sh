fc56b5ead94d3ce6e12668db4a84e3f33dedd3ac
Anto <42590059+RepositoriumCodice@users.noreply.github.com>
2019-12-28 12:16:13 +1100

    Update backupNextCloud.sh

--- Linux/Docker-NextCloud/backupNextCloud.sh
+++ Linux/Docker-NextCloud/backupNextCloud.sh
@@ -4,10 +4,11 @@ NC_DB_USER=nextcloud
 NC_DB_PASS=password
 NC_DB_NAME=nextcloud
 NC_DB_TYPE=mysql
-NC_DB_DUMP_DIR=/mnt/containers/mysql/
+NC_DB_DUMP_DIR=/mnt/containers/nextcoud/backups #no ending /
 NC_APP_CONTAINER=nextcloud
 NC_DB_CONTAINER=mysql
 
+mkdir -p $NC_DB_DUMP_DIR
 # abort entire script if any command fails
 set -e
 
@@ -18,12 +19,10 @@ trap "docker exec -u www-data $NC_APP_CONTAINER php occ maintenance:mode --off" 
 docker exec -u www-data $NC_APP_CONTAINER php occ maintenance:mode --on
 
 # backup db
-sudo docker exec $NC_DB_CONTAINER mysqldump --single-transaction -h localhost -u $NC_DB_USER -p$NC_DB_PASS $NC_DB_NAME > $NC_DB_DUMP_DIR/db_dump_$NC_DB_TYPE_nextcloud.sql
+sudo docker exec $NC_DB_CONTAINER mysqldump --single-transaction -h localhost -u $NC_DB_USER -p$NC_DB_PASS $NC_DB_NAME > $NC_DB_DUMP_DIR/db_dump_$NC_DB_TYPE"_nextcloud.sql"
 
 # end maintenance mode
 docker exec -u www-data $NC_APP_CONTAINER php occ maintenance:mode --off
 
 # delete trap
 trap "" EXIT
-
-
