81dc138b3e5f8f08c43f12b2397e52842b6af13d
Anto <42590059+RepositoriumCodice@users.noreply.github.com>
2019-12-28 13:38:17 +1100

    Update backupNextCloud.sh

--- Linux/Docker-NextCloud/backupNextCloud.sh
+++ Linux/Docker-NextCloud/backupNextCloud.sh
@@ -11,15 +11,10 @@
 # run:
 # ./backupNextcloud.sh 
 
-NC_DB_USER=nextcloud
-NC_DB_PASS=password
-NC_DB_NAME=nextcloud
-NC_DB_TYPE=mysql
-NC_DB_DUMP_DIR=/mnt/containers/nextcoud/backups #no ending /
-NC_APP_CONTAINER=nextcloud
-NC_DB_CONTAINER=mysql
+# config
+source config.sh
 
-mkdir -p $NC_DB_DUMP_DIR
+mkdir -p $NC_BACKUP_DIR
 # abort entire script if any command fails
 set -e
 
@@ -30,7 +25,13 @@ trap "docker exec -u www-data $NC_APP_CONTAINER php occ maintenance:mode --off" 
 docker exec -u www-data $NC_APP_CONTAINER php occ maintenance:mode --on
 
 # backup db
-sudo docker exec $NC_DB_CONTAINER mysqldump --single-transaction -h localhost -u $NC_DB_USER -p$NC_DB_PASS $NC_DB_NAME > $NC_DB_DUMP_DIR/db_dump_$NC_DB_TYPE"_nextcloud.sql"
+sudo docker exec $NC_DB_CONTAINER mysqldump --single-transaction -h localhost -u $NC_DB_USER -p$NC_DB_PASS $NC_DB_NAME > $NC_BACKUP_DIR/db_dump_$NC_DB_TYPE.sql
+tar -zcvf $NC_BACKUP_DIR/dbDump.tar.gz $NC_BACKUP_DIR/db_dump_$NC_DB_TYPE.sql
+aws s3 cp $NC_BACKUP_DIR/dbDump.tar.gz $S3_BUCKET --profile=$AWS_CLI_PROFILE
+
+#backup files
+tar -zcvf $NC_BACKUP_DIR/userData.tar.gz $NC_DATA_DIR
+aws s3 cp $NC_BACKUP_DIR/userData.tar.gz $S3_BUCKET --profile=$AWS_CLI_PROFILE
 
 # end maintenance mode
 docker exec -u www-data $NC_APP_CONTAINER php occ maintenance:mode --off
