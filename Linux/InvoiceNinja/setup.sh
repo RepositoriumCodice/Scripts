72b6bbdf93816458118d73209adecc2c6b51091d
Anto <42590059+RepositoriumCodice@users.noreply.github.com>
2021-03-06 21:11:23 +1100

    Update setup.sh

--- Linux/InvoiceNinja/setup.sh
+++ Linux/InvoiceNinja/setup.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
+
+# Install required apps
 apt-get update
-apt-get install -y wget unzip nano
+apt-get install -y wget unzip
 docker-php-ext-install mysqli pdo pdo_mysql
 mkdir -p /var/www/
+
+# Download Invoice Ninja
 cd /tmp/
 wget -O invoice-ninja.zip https://download.invoiceninja.com/
+
+# Unzip and set Invoice Ninja permissions
 unzip invoice-ninja.zip -d /var/www/
 mv /var/www/ninja /var/www/invoice-ninja
 chown www-data:www-data /var/www/invoice-ninja/ -R
 chmod 755 /var/www/invoice-ninja/storage/ -R
 
+# Add Invoice Ninja Virtual Host
 echo "<VirtualHost *:80>
-    #ServerName invoice.yourdomain.com
     DocumentRoot /var/www/invoice-ninja/public
 
     <Directory /var/www/invoice-ninja/public>
@@ -24,6 +30,8 @@ echo "<VirtualHost *:80>
     ErrorLog ${APACHE_LOG_DIR}/invoice-ninja.error.log
     CustomLog ${APACHE_LOG_DIR}/invoice-ninja.access.log combined
 </VirtualHost>" >> /etc/apache2/sites-available/invoice-ninja.conf
+
+# Adjust Apache config and restart
 a2dissite 000-default.conf
 a2ensite invoice-ninja.conf
 a2enmod rewrite
